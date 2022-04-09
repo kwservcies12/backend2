98c15091643b9b58fc0cc9d045461fa9de4b75aa
kwservices12 <kwservices12@gmail.com>
2022-04-09 12:23:38 -0400

    j

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+	return (
+		<footer>
+			<Container>
+				<Row>
+					<Col className='text-center py-3'>
+						{/* Kane Whiteside */}
+						<a
+							rel='noopener noreferrer'
+							href='https://github.com/kwservices'
+							target='_blank'
+							className='kane-whiteside'
+						>
+							<i className='fas fa-user-circle'></i> Kane Whiteside Github
+						</a>
+						Copyright &copy; React E-Commerce
+					</Col>
+				</Row>
+			</Container>
+		</footer>
+	)
+}
+
+export default Footer
